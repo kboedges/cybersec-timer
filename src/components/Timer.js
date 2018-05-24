e601aa110308624b35046e5d2b8bee86ee72acda
Katherine Boedges <kboedges@me.com>
2018-05-24 11:27:06 -0500

    Adding notes

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,3 +1,5 @@
+// Turn timeLeft into an object and then have an if statement for each one and use the comment below to generate a div for hours, mins, secs.
+
 import React, { Component } from 'react';
 
 let timeLeft = "04:59:59";
@@ -8,9 +10,14 @@ class Timer extends Component {
     return (
       <div className="timer">
       <h1 className="timer-head text-center mt-4 mb-5">
+
+        
+        
         {timeLeftArray.map((char, index) => (
           <span key={index} className="time-char px-3 py-3 mx-2">{char}</span>
         ))}
+      
+      
       </h1>
         
       </div>
