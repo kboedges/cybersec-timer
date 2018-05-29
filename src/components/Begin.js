d6d73068fdf3273f11f3aa94af0cd681eb335fc7
Katherine Boedges <kboedges@me.com>
2018-05-29 17:05:20 -0500

    Formatting

--- src/components/Begin.js
+++ src/components/Begin.js
@@ -9,11 +9,11 @@ class Begin extends Component {
     
     return (
       <div className="begin">
-       <div className="box p-5 d-flex d-flex flex-column justify-content-center align-items-center">
+       <form className="box p-5 d-flex d-flex flex-column justify-content-center align-items-center">
         <h1 className="text-center m-3">Begin your challenge.</h1>
         <input className="form-control w-50 m-3" type="text" placeholder="Team Name"/>
         <button className="m-3 btn btn-lg btn-cornflower px-5" onClick={() => startTimer("Cool Team")}>{timerStarted ? "Continue" : "Start"}</button>
-      </div>
+      </form>
     </div>
     );
   }
