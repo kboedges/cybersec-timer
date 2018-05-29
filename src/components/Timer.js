480bab2bfa6ca5c7105a71a040f1b8f4c608033c
Katherine Boedges <kboedges@me.com>
2018-05-29 11:04:36 -0500

    Adjusting clock initial state

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,41 +1,38 @@
-// Turn timeLeft into an object and then have an if statement for each one and use the comment below to generate a div for hours, mins, secs.
-
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
 import { STOP_TIMER, UPDATE_TIME } from '../actions';
 
 function hmsCalculator(t) {
-  const seconds = Math.floor( (t.total/1000) % 60 );
-  const minutes = Math.floor( (t.total/1000/60) % 60 );
-  const hours = Math.floor( (t.total/(1000*60*60)) % 24 );
-  console.log(t);
-  return `${('0' + hours).slice(-2)}:${('0' + minutes).slice(-2)}:${('0' + seconds).slice(-2)}`.split("");
+  return `${('0' + t.hours).slice(-2)}:${('0' + t.minutes).slice(-2)}:${('0' + t.seconds).slice(-2)}`.split("");
 }
-  
 
-let timeLeft = "04:59:59";
-let timeLeftArray = timeLeft.split("");
+// componentDidMount()
+// function initializeClock(id, endtime){
+//   var clock = document.getElementById(id);
+//   var timeinterval = setInterval(function(){
+//     var t = getTimeRemaining(endtime);
+//     clock.innerHTML = `${('0' + t.hours).slice(-2)} : ${('0' + t.minutes).slice(-2)} : ${('0' + t.seconds).slice(-2)}`;
+    
+//     if(t.total <= 0){
+//       clearInterval(timeinterval);
+//     }
+//   },1000);
+// }
+// initializeClock('clockdiv', deadline);
 
 class Timer extends Component {
 
   render() {
     const { state } = this.props;
-    console.log(hmsCalculator(state.timeReducer.timeLeft));
     
     return (
       <div className="timer pb-3">
-      <p className="text-center pb-3">Your data will be deleted in:</p>
-      <h1 className="timer-head text-center mb-5">
-
-        
-        
-        {hmsCalculator(state.timeReducer.timeLeft).map((char, index) => (
-          <span key={index} className="time-char px-3 py-3 mx-2">{char}</span>
-        ))}
-      
-      
-      </h1>
-        
+        <p className="text-center pb-3">Your data will be deleted in:</p>
+        <h1 className="timer-head text-center mb-5">
+          {hmsCalculator(state.timeReducer.timeLeft).map((char, index) => (
+            <span key={index} className="time-char px-3 py-3 mx-2">{char}</span>
+          ))}
+        </h1>
       </div>
     );
   }
@@ -46,6 +43,5 @@ const mapStateToProps = state => {
     state
   }
 }
-
 
 export default connect(mapStateToProps)(Timer);
--- src/reducers/timeReducer.js
+++ src/reducers/timeReducer.js
@@ -3,12 +3,13 @@ import { START_TIMER, STOP_TIMER, UPDATE_TIME } from '../actions';
 const initialTimer = {
     timerStarted: false,
     timeLeft: {
-        total: 5*60*60*1000,
-        seconds: Math.floor( (this.total/1000) % 60 ),
-        minutes: Math.floor( (this.total/1000/60) % 60 ),
-        hours: Math.floor( (this.total/(1000*60*60)) % 24 )
+        total: 5*60*60*1000
     }
 }
+
+initialTimer.timeLeft.seconds = Math.floor((initialTimer.timeLeft.total/1000) % 60);
+initialTimer.timeLeft.minutes = Math.floor((initialTimer.timeLeft.total/1000/60) % 60);
+initialTimer.timeLeft.hours = Math.floor((initialTimer.timeLeft.total/(1000*60*60)) % 24);
 
 function timeReducer(state = initialTimer, action) {
     switch(action.type) {
