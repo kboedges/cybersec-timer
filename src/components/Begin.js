3103712b03120950ff7bd861c4cfcb2beaf1f565
Katherine Boedges <kboedges@me.com>
2018-05-29 17:08:05 -0500

    Adding support for the teamName input

--- src/components/Begin.js
+++ src/components/Begin.js
@@ -3,6 +3,15 @@ import {connect} from 'react-redux'
 import { startTimer } from '../actions';
 
 class Begin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
 
   render() {
     const {timerStarted, startTimer} = this.props;
@@ -11,8 +20,8 @@ class Begin extends Component {
       <div className="begin">
        <form className="box p-5 d-flex d-flex flex-column justify-content-center align-items-center">
         <h1 className="text-center m-3">Begin your challenge.</h1>
-        <input className="form-control w-50 m-3" type="text" placeholder="Team Name"/>
-        <button className="m-3 btn btn-lg btn-cornflower px-5" onClick={() => startTimer("Cool Team")}>{timerStarted ? "Continue" : "Start"}</button>
+        <input className="form-control w-50 m-3" value={this.state.value} onChange={this.handleChange} type="text" placeholder="Team Name"/>
+        <button className="m-3 btn btn-lg btn-cornflower px-5" onClick={() => startTimer(this.state.value)}>{timerStarted ? "Continue" : "Start"}</button>
       </form>
     </div>
     );
