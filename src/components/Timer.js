7a856a54e9aa69a2cadf212988bc3917134edd16
Katherine Boedges <kboedges@me.com>
2018-05-30 14:48:55 -0500

    Implementing a coundown

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux'
-import { STOP_TIMER, UPDATE_TIME } from '../actions';
+import Countdown from 'react-countdown-now';
 
-function hmsCalculator(t) {
-  return `${('0' + t.hours).slice(-2)}:${('0' + t.minutes).slice(-2)}:${('0' + t.seconds).slice(-2)}`.split("");
-}
+// function hmsCalculator(t) {
+//   return `${('0' + t.hours).slice(-2)}:${('0' + t.minutes).slice(-2)}:${('0' + t.seconds).slice(-2)}`.split("");
+// }
 
 // function initializeClock(endtime){
 //     var timeinterval = setInterval(function(){
@@ -22,26 +21,39 @@ class Timer extends Component {
   // }
 
   render() {
-    const { timeLeft } = this.props;
-    console.log(timeLeft)
+    // const { timeLeft } = this.props;
+    // console.log(timeLeft)
     
     return (
       <div className="timer pb-3">
         <p className="text-center pb-3">Your data will be deleted in:</p>
         <h1 className="timer-head text-center mb-5">
-          {hmsCalculator(timeLeft).map((char, index) => (
+          {/* {hmsCalculator(timeLeft).map((char, index) => (
             <span key={index} className="time-char px-3 py-3 mx-2">{char}</span>
-          ))}
+          ))} */}
+
+          <Countdown 
+            date={Date.now() + 5*60*60*1000} 
+            renderer={props => (
+            <div>{`${props.hours}:${props.minutes}:${props.seconds}`.split("").map((char, index) => (
+              <span key={index} className="time-char px-3 py-3 mx-2">{char}</span>
+            ))}</div>
+            )}
+          />
+
+          
+        
         </h1>
       </div>
     );
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    timeLeft: state.timeReducer.timeLeft
-  }
-}
+// const mapStateToProps = state => {
+//   return {
+//     timeLeft: state.timeReducer.timeLeft
+//   }
+// }
 
-export default connect(mapStateToProps)(Timer);
+// export default connect(mapStateToProps)(Timer);
+export default Timer;
