342541a9cdd4daa229ed201d2ced14331eb2e500
Katherine Boedges <kboedges@me.com>
2018-05-30 13:18:46 -0500

    Starting timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -6,30 +6,30 @@ function hmsCalculator(t) {
   return `${('0' + t.hours).slice(-2)}:${('0' + t.minutes).slice(-2)}:${('0' + t.seconds).slice(-2)}`.split("");
 }
 
-// componentDidMount()
-// function initializeClock(id, endtime){
-//   var clock = document.getElementById(id);
-//   var timeinterval = setInterval(function(){
-//     var t = getTimeRemaining(endtime);
-//     clock.innerHTML = `${('0' + t.hours).slice(-2)} : ${('0' + t.minutes).slice(-2)} : ${('0' + t.seconds).slice(-2)}`;
-    
-//     if(t.total <= 0){
-//       clearInterval(timeinterval);
-//     }
-//   },1000);
-// }
-// initializeClock('clockdiv', deadline);
+// function initializeClock(endtime){
+//     var timeinterval = setInterval(function(){
+//       var t = getTimeRemaining(endtime);
+//       if(t.total <= 0){
+//         clearInterval(timeinterval);
+//       }
+//     },1000);
+//   }
 
 class Timer extends Component {
 
+  // componentDidMount(){
+  //   initializeClock('clockdiv', deadline);
+  // }
+
   render() {
-    const { state } = this.props;
+    const { timeLeft } = this.props;
+    console.log(timeLeft)
     
     return (
       <div className="timer pb-3">
         <p className="text-center pb-3">Your data will be deleted in:</p>
         <h1 className="timer-head text-center mb-5">
-          {hmsCalculator(state.timeReducer.timeLeft).map((char, index) => (
+          {hmsCalculator(timeLeft).map((char, index) => (
             <span key={index} className="time-char px-3 py-3 mx-2">{char}</span>
           ))}
         </h1>
@@ -40,7 +40,7 @@ class Timer extends Component {
 
 const mapStateToProps = state => {
   return {
-    state
+    timeLeft: state.timeReducer.timeLeft
   }
 }
 
